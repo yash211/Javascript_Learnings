8b9a4f6367337a93c7ad0701d050c3fef3dde8de
yash211 <gupta.yash211@gmail.com>
2022-11-04 19:19:26 +0530

    gulp_learning

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,45 @@
 const gulp=require('gulp');
 const ugly=require('gulp-uglify');
+var jshint=require('gulp-jshint');   //useful for debugging js files
+var changed=require('gulp-changed');
+var plumber=require('gulp-plumber'); //Complete all the task and prevents gulp tasks from stopping if there are any errors
+var minCSS=require('gulp-clean-css')
 
-gulp.task('minify',async function(){
-  gulp.src('script.js')
-      .pipe(ugly())
-      .pipe(gulp.dest('js'))
+var SRC='script.js'
+var DES='js'
+
+//Update only the changed js files
+gulp.task('changed',function(){
+  return gulp.src(SRC)
+  .pipe(changed(DES))
+  .pipe(gulp.dest(DES))
+})
+
+//kind of realtime debugger function
+gulp.task('jshint',function(){
+  gulp.src(SRC) 
+  .pipe('plumber')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
 });
 
+//minify js file 
+gulp.task('minify',async function(){
+  gulp.src(SRC)
+      .pipe(ugly())
+      .pipe(gulp.dest(DES))
+});
+
+//minify css 
+gulp.task('cleancss',function(){
+  return gulp.src('style.css')
+  .pipe(minCSS())
+  .pipe(gulp.dest('css'))
+});
+
+//watch task-executes without command
+gulp.task('watch',function(){
+  gulp.watch(SRC,gulp.series('changed'));
+});
+
+gulp.task('default',gulp.series('watch','jshint'));  //runs all task one time only
